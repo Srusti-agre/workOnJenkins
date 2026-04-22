4b5914bb91e96b9e00c6cebbfda9a33008a89dc2
Srusti Agre <srustiagre9@gmail.com>
2026-04-22 22:23:08 +0530

    added pipeline

---
+++ jenkins/uptime_server.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+    stages {
+        stage("info") {
+            steps {
+                sh """
+                    pwd
+                    whoami
+                    uptime
+                    echo $JOB_NAME
+                    echo $BUILD_ID
+                """
+            }
+        }
+    }
+}
